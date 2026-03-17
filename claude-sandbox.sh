2e1be86af630880222cc05bf7af450e90d586969
Andrés Añez <aeanez@gmail.com>
2026-03-17 10:14:19 -0300

    Mount /usr/lib and /etc/manpath.config, override PS1 via PROMPT_COMMAND
    - Mount /usr/lib:ro to cover all host binary library deps (replaces
      individual git-core mount)
    - Mount /etc/manpath.config:ro to fix manpath startup error
    - Set PROMPT_COMMAND to override PS1 inside the container so the
      prompt shows the container hostname instead of hardcoded values

--- claude-sandbox.sh
+++ claude-sandbox.sh
@@ -9,18 +9,20 @@ _claude_docker() {
     --hostname "${SANDBOX_HOSTNAME:-sandbox}" \
     --network host \
     -e HOME="$HOME" \
-    -e PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/host/bin:/host/lib/git-core:$HOME/.local/bin" \
+    -e PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/host/bin:$HOME/.local/bin" \
+    -e PROMPT_COMMAND='PS1="\[\033[1;36m\]\h\[\033[0m\]:\[\033[1;33m\]\w\[\033[0m\]\[\033[1;32m\]$(parse_git_branch 2>/dev/null)\[\033[0m\]\[\033[1;37m\]\$ \[\033[0m\]"' \
     -v "$HOME:$HOME" \
     -v "$HOME/.ssh:$HOME/.ssh:ro" \
     -v "$HOME/.aws:$HOME/.aws:ro" \
     -v "$HOME/.gnupg:$HOME/.gnupg:ro" \
     -v /lib/x86_64-linux-gnu:/lib/x86_64-linux-gnu:ro \
+    -v /usr/lib:/usr/lib:ro \
     -v /usr/bin:/host/bin:ro \
-    -v /usr/lib/git-core:/host/lib/git-core:ro \
     -v "$(readlink -f /usr/bin/docker):/usr/bin/docker:ro" \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
+    -v /etc/manpath.config:/etc/manpath.config:ro \
     -w "$PWD" \
     ubuntu:22.04 \
     "$@"
